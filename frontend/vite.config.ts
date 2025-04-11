f27fc3d04b33b695116656b99670b97c7af1d84d
AlanRYP Souza <alansouzeesi@gmail.com>
2025-04-11 11:24:20 -0300

    fix: little changes on dist and vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-import path from "node:path";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
+  base: '/',
   resolve: {
     alias: {
       "@assets": path.resolve(__dirname, './src/assets'),
@@ -11,8 +11,7 @@ export default defineConfig({
       "@components": path.resolve(__dirname, './src/components'),
       "@store": path.resolve(__dirname, './src/store'),
       "@reducers": path.resolve(__dirname, './src/reducers'),
-    }
+    },
   },
   plugins: [react()],
-  base: '/', 
-})
+});
