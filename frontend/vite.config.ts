a221c1e14840959389cf7edfdafb496c9c3791b2
AlanRYP Souza <alansouzeesi@gmail.com>
2025-04-11 11:28:16 -0300

    fix: little changes

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,17 +1,17 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+import path from "node:path";
 
 export default defineConfig({
-  base: '/',
-  resolve: {
-    alias: {
+  resolve:{
+    alias:{
       "@assets": path.resolve(__dirname, './src/assets'),
       "@pages": path.resolve(__dirname, './src/pages'),
       "@components": path.resolve(__dirname, './src/components'),
       "@store": path.resolve(__dirname, './src/store'),
       "@reducers": path.resolve(__dirname, './src/reducers'),
-    },
+    }
   },
   plugins: [react()],
-});
+})
